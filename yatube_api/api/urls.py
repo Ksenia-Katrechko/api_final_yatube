22a8c59bddc510f6f7d316746da1c16bba1cbe85
Ксения Катречко <stas.basov@whatever.com>
2024-02-21 16:52:54 +0200

    Роман, приветствую! Жду комментарии, спасибо!

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,20 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from api.views import (CommentViewSet,
+                       GroupViewSet, PostViewSet,
+                       FollowViewSet,)
+
+router_version1 = DefaultRouter()
+router_version1.register('posts', PostViewSet, basename='posts')
+router_version1.register('groups', GroupViewSet, basename='groups')
+router_version1.register(
+    'posts/(?P<post_id>\\d+)/comments',
+    CommentViewSet,
+    basename='comment')
+router_version1.register('follow', FollowViewSet, basename='followers')
 
 urlpatterns = [
+    path('v1/', include(router_version1.urls)),
+    path('v1/', include('djoser.urls.jwt')),
 ]
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,23 +4,70 @@ from django.db import models
 User = get_user_model()
 
 
-class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
-    image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
+MAX_TEXT_LENGHT = 50
 
-    def __str__(self):
-        return self.text
+
+class Group(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    slug = models.SlugField(unique=True, verbose_name='Slug')
+    description = models.TextField(verbose_name='Описание')
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
+    def _str_(self):
+        return self.title[:MAX_TEXT_LENGHT]
+
+
+class Post(models.Model):
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='posts', verbose_name='Автор')
+    image = models.ImageField(upload_to='posts/', null=True, blank=True,
+                              verbose_name='Изображение')
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL,
+                              related_name='posts', blank=True,
+                              null=True, verbose_name='Группа')
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+    def _str_(self):
+        return self.text[:MAX_TEXT_LENGHT]
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
-    created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='comments', verbose_name='Автор')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='comments', verbose_name='Пост')
+    text = models.TextField(verbose_name='Текст')
+    created = models.DateTimeField('Дата добавления', auto_now_add=True,
+                                   db_index=True)
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def _str_(self):
+        return self.text[:MAX_TEXT_LENGHT]
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='follower',
+        verbose_name='Подписчик')
+    following = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='following',
+        verbose_name='Подписка')
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('user', 'following'),
+                name='unique_follow'
+            ),
+        )
